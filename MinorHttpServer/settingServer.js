8031b1cdb23559f5096ef981c02347733fbf5408
cex <cex@cexs-MBP.lan>
2017-11-27 19:22:38 +0000

    Update

--- MinorHttpServer/settingServer.js
+++
@@ -1,7 +0,0 @@
-var http_port = 6001;
-var p2p_port = 3001;
-var initialPeersSockets = [];
-
-exports.http_port = http_port;
-exports.p2p_port = p2p_port;
-exports.initialPeersSockets = initialPeersSockets;
